f2ab6fc7eac5f1bbef42d6b6148fb21f476f0a24
Nitya Narasimhan <nitya@users.noreply.github.com>
2022-08-08 10:36:51 -0400

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Nitya Narasimhan',
+    site_url: 'https://next.nitya.dev',
+    feed_url: 'https://next.nitya.dev/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
